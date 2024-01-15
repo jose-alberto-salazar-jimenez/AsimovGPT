538749ec9ca4897b8318b68de2e22cb2a3955ee0
jose-alberto salazar-jimenez <jasj.1991@gmail.com>
2024-01-14 20:25:10 -0600

    Starting project

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+def main():
+    st.set_page_config(page_title='chat with pdfs', page_icon=':books:')
+    st.header('chat with multiple pdfs :books:')
+    st.text_input("ask a question about Isaac Asimov's Sci-Fi Universe*:")
+
+    with st.sidebar:
+        st.subheader('Your Documents')
+        st.file_uploader('Upload pdfs and click on process')
+        st.button('Process')
+
+
+
+if __name__=='__main__':
+    main()
